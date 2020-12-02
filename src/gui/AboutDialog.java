cf173d8da007d30f5cc9afa6759f6b6f5091330f
djordjekrsmanovic <krsmanovic.ra71.2018@uns.ac.rs>
2020-12-02 18:58:12 +0100

    #about_dialog uklanjanje nepotrebnih importova

--- src/gui/AboutDialog.java
+++ src/gui/AboutDialog.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
