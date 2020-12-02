a82b032e81ed8e61674e44d186fa39e3ff6229a1
ra74-2018 <nikolajovisic@uns.ac.rs>
2020-12-02 18:41:27 +0100

    #main
    Dodat Main.

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import gui.MainWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainWindow glavniProzor = MainWindow.getInstance();
+		glavniProzor.setVisible(true);
+	}
+
+}
