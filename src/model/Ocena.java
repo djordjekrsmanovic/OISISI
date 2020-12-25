4964984cc108ab2bf2a85ad3194e4943dbe6b255
djordjekrsmanovic <krsmanovic.ra71.2018@uns.ac.rs>
2020-12-25 15:28:43 +0100

    #ocena dodavanje klase Ocena

---
+++ src/model/Ocena.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.Date;
+
+public class Ocena {
+	private Student s;
+	private Predmet p;
+	private int vrijednostOcjene;
+	private Date datumPolaganja;
+	public Ocena(Student s, int vrijednostOcjene, Date datumPolaganja) {
+		super();
+		this.s = s;
+		this.vrijednostOcjene = vrijednostOcjene;
+		this.datumPolaganja = datumPolaganja;
+	}
+	public Predmet getP() {
+		return p;
+	}
+	public void setP(Predmet p) {
+		this.p = p;
+	}
+	public Student getS() {
+		return s;
+	}
+	public void setS(Student s) {
+		this.s = s;
+	}
+	public int getVrijednostOcjene() {
+		return vrijednostOcjene;
+	}
+	public void setVrijednostOcjene(int vrijednostOcjene) {
+		this.vrijednostOcjene = vrijednostOcjene;
+	}
+	public Date getDatumPolaganja() {
+		return datumPolaganja;
+	}
+	public void setDatumPolaganja(Date datumPolaganja) {
+		this.datumPolaganja = datumPolaganja;
+	}
+	
+	
+	
+}
